99eba44b01c6837bf753f775bb0f77306ceaab0b
yoonseong <ysc77135@gmail.com>
2021-07-19 16:24:31 +0900

    init solution

---
+++ src/Algorithm.java
@@ -0,0 +1,7 @@
+public class Algorithm {
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    System.out.println(solution.solution());
+  }
+}
---
+++ src/Solution.java
@@ -0,0 +1,6 @@
+class Solution {
+
+  public int solution() {
+    return 0;
+  }
+}
