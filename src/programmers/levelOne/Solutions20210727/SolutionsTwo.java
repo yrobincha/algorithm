6c9d92a853700b4d65391dda1a2b0a22d63521db
yoonseong <ysc77135@gmail.com>
2021-07-27 17:30:25 +0900

    20210727_level1_2

---
+++ src/programmers/levelOne/Solutions20210727/SolutionsTwo.java
@@ -0,0 +1,16 @@
+package programmers.levelOne.Solutions20210727;
+
+public class SolutionsTwo {
+
+  public String solution(int n) {
+    StringBuilder answer = new StringBuilder();
+    for (int i = 0; i < n; i++) {
+      if (i % 2 == 0) {
+        answer.append("수");
+      } else {
+        answer.append("박");
+      }
+    }
+    return answer.toString();
+  }
+}
