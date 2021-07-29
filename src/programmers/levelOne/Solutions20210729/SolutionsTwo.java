230ae35aad2e851ff45d207ef1a9784d4da04497
yoonseong <ysc77135@gmail.com>
2021-07-29 11:11:10 +0900

    20210729_level1_2

---
+++ src/programmers/levelOne/Solutions20210729/SolutionsTwo.java
@@ -0,0 +1,12 @@
+package programmers.levelOne.Solutions20210729;
+
+public class SolutionsTwo {
+
+  public String solution(int num) {
+    if (num % 2 == 0) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
+  }
+}
